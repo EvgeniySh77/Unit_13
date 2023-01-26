e480135ef5a713c23f001ab643141e79ca58b7c6
Шарафутдинов Евгений Асгатович <evgen.sh77@mail.ru>
2023-01-26 22:27:25 +0300

    Добавьте файлы проекта.

---
+++ Task_13_6_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace Task_13_6_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var path = @"C:/Users/Public/Documents/Text1.txt";
+            var text = File.ReadAllText(path);            
+
+            char[] delimiterChars = { ' ', '\t', '\n', '\r' };
+            string[] words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);                     
+
+            var list = new List<string>();
+            var linkedList = new LinkedList<string>();
+
+            var measuringList = new Stopwatch();
+            foreach (var line in words)
+            {
+                measuringList.Start();
+                list.Add(line);
+                measuringList.Stop();                
+            }            
+
+            var measuringLinkedList = new Stopwatch();
+            foreach (var line in words)
+            {
+                measuringLinkedList.Start();    
+                linkedList.AddLast(line);
+                measuringLinkedList.Stop();                
+            }
+            
+            Console.WriteLine($"Число элементов в структуре данных List({list.Count}) и LinkedList({linkedList.Count}) равны.");
+            Console.WriteLine($"Время вставки элементов в List = {measuringList.ElapsedMilliseconds} мс.");
+            Console.WriteLine($"Время вставки элементов в LinkedList = {measuringLinkedList.ElapsedMilliseconds} мс.");
+            Console.WriteLine($"Вставка элементов в List происходит быстрее чем в LinkedList, примерно в " +
+                $"{measuringLinkedList.ElapsedMilliseconds / measuringList.ElapsedMilliseconds} раз(а)...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_13_6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Task_13_6_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var frequentWords = new List<string>();
+            var path = @"C:/Users/Public/Documents/Text1.txt";
+            var text = File.ReadAllText(path);                        
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());            
+
+            char[] delimiterChars = { ' ', '\t', '\n', '\r' };
+
+            string[] words = noPunctuationText.ToUpper().Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            frequentWords = words.Where(g => g.Length > 2).GroupBy(g => g)
+                .OrderByDescending(g => g.Count()).Select(g => g.Key).Take(10).ToList();
+
+            Console.WriteLine("10 слов чаще всего встречающихся в тексте и состоящих из более двух букв:");
+            foreach (var line in frequentWords)
+            {                
+                Console.Write(line + " ");
+            }           
+
+            Console.ReadKey();
+        }
+    }
+}
